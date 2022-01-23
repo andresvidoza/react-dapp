76dfbe5285c167e9060de076465673e4bd380ec6
andresvidoza <andresvidoza@gmail.com>
2022-01-23 11:39:05 -0500

    Using infura to deploy this smart contract to a test network like ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv/config");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -24,6 +25,10 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 1337 // specific to how hardhat works.
+    },
+    ropsten: {
+      url: `${process.env.ROPTSTEN_URL}`,
+      accounts: [`0x${process.env.ACCOUNT_KEY}`] // deploying this account on behalf of this user thats why we use the private key
     }
   }
 
