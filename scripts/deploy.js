de0a2c21c637c2ac7c49245729a4703c84b3fdfa
andresvidoza <andresvidoza@gmail.com>
2022-01-23 15:07:54 -0500

    making sure I can mint from the UI react in order to buy a token and put it in my metamask wallet from ropsten

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,22 @@
 const hre = require("hardhat");
 
 async function main() {
-  // reference to the contract we want to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  // pass the constructor text we want to pass in
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  // // reference to the contract we want to deploy
+  // const Greeter = await hre.ethers.getContractFactory("Greeter");
+  // // pass the constructor text we want to pass in
+  // const greeter = await Greeter.deploy("Hello, Hardhat!");
 
-  // wait for this to actually be deployed
-  await greeter.deployed();
+  // // wait for this to actually be deployed
+  // await greeter.deployed();
 
-  // console log it, and the actual address we will be deploying too. 
-  console.log("Greeter deployed to:", greeter.address);
+  // // console log it, and the actual address we will be deploying too. 
+  // console.log("Greeter deployed to:", greeter.address);
+
+  //Contract Deployment for erc721
+  const POTI = await hre.ethers.getContractFactory("PeopleOfTheInternet");
+  const poti = await POTI.deploy();
+  await poti.deployed();
+  console.log("People of the Internet deployed to:", poti.address);
 }
 
 // treat your promise
--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import { useState } from 'react';
 import { ethers } from 'ethers';
 import './App.css';
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+import POTI from './artifacts/contracts/POTI.sol/PeopleOfTheInternet.json'
+import { getAccountPath } from 'ethers/lib/utils';
+import { Web3Provider } from '@ethersproject/providers';
 
 // Store contract within just a variable
-const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const potiAddress = "0xAedF2bad6D60357Aa2F1e0f05879018106E30354";
 
 function App() {
 
   // set state - “array destructuring”. 
   const [greeting, setGreetingValue] = useState(""); // Declare a new state variable, which we'll call "greeting"
+  const [index, setOwnerIndex] = useState(""); // Declare a new state variable, which we'll call "greeting"
 
   // connect to metamask wallet of user
   async function requestAccount(){
@@ -61,15 +66,67 @@ function App() {
     }
   }
 
+  // Minting my NFT
+  async function mintNFT(){
+    if(typeof window.ethereum !== 'undefined'){
+      // wait for user to enable account to be used and connect wallet
+      await requestAccount(); // giving access to wallet
+      // create a new provider - A Provider is an abstraction of a connection to the Ethereum network, providing a concise, consistent interface to standard Ethereum node functionality.
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const gasPrice = await provider.getGasPrice();
+      // sign transaction using a signer, cause we will create an update in the blockchain
+      const signer = provider.getSigner();
+      // await signer.sendTransaction({
+      //   to: potiAddress,
+      //   value: ethers.utils.parseEther("0.01"),
+      // });
+      const contract = new ethers.Contract(potiAddress, POTI.abi, signer);
+      //now we can create the transaction ( writing )
+      const transaction = await contract.safeMint(window.ethereum.selectedAddress, "https//ipfs.io/ipfs/QmZjnN9TJZAzMGAHoxKLwsLM9Z3awMaFFK9BMByvEDxsCy", {value: ethers.utils.parseEther("0.01"), gasPrice: ethers.utils.parseUnits('100', 'gwei'), gasLimit: 1000000}); // safe mint to this address
+      // wait for the transaction to be confirmed in the blockchain ( in production environment in takes longer )
+      await transaction.wait();
+
+    }
+  }
+
+  async function ownerOf(){
+    // if metamask extension is connected (metamask connection ejects window.ethereum into the object)
+    if(typeof window.ethereum !== 'undefined'){
+      // create a new provider - A Provider is an abstraction of a connection to the Ethereum network, providing a concise, consistent interface to standard Ethereum node functionality.
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      // create instance of the contract
+      const contract = new ethers.Contract(potiAddress, POTI.abi, provider); // smart contract address, info and use the layer to talk
+
+      console.log(contract)
+
+      try{
+        // use the contract data here
+        const data = await contract.ownerOf(index);
+        console.log('data: ', data);
+      }catch(err){
+        console.log("Error: ", err)
+      }
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={fetchGreeting}> Fetch Greeting</button>
         <button onClick={setGreeting}> Set Greeting</button>
+        <button onClick={mintNFT}> Mint</button>
+        <button onClick={ownerOf}> Owner off</button>
+
         <input
           onChange={ e => setGreetingValue(e.target.value)}
           placeholder='Set greeting'
           value={greeting}
+        />
+
+      <input
+          onChange={ e => setOwnerIndex(e.target.value)}
+          placeholder='Set owner index'
+          value={index}
         />
       </header>
     </div>
