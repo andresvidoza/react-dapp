b3baa2c1dccf9725b01adeb600717161d300aef0
andresvidoza <andresvidoza@gmail.com>
2022-01-23 13:06:04 -0500

    Adding my ERC721 smart contract created and tested on Remix

---
+++ contracts/POTI.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract PeopleOfTheInternet is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uint256 public mintRate = 0.01 ether;
+    uint public MAX_SUPPLY = 1; // only one minting
+
+    constructor() ERC721("PeopleOfTheInternet", "POTI") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https//ipfs.io/ipfs/QmZjnN9TJZAzMGAHoxKLwsLM9Z3awMaFFK9BMByvEDxsCy";
+    }
+
+    // anybody can call and create their own NFT, mint can go forever ( add onlyOwner if you dont want)
+    function safeMint(address to, string memory _tokenURI) public payable { // now you need to pay for it 
+        require(totalSupply() < MAX_SUPPLY, "Can't mint more!");
+        require(msg.value >= mintRate, "Not enough ether sent.");
+        // start at 1
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    // only owner who deployed contract can use it - widthdrawal of NFT
+    function withdraw() public onlyOwner{
+        require(address(this).balance >0, "Balance is 0");
+        payable(owner()).transfer(address(this).balance);
+    }
+}
